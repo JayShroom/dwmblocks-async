4ffd4de7127d90d253b88d7f44329d793890190d
Jay Shroomie <113574995+JayShroom@users.noreply.github.com>
2023-01-05 17:40:10 +1300

    Add files via upload

---
+++ dwmblocks-async/config.h
@@ -0,0 +1,11 @@
+#define CMDLENGTH 45
+#define DELIMITER "  "
+#define CLICKABLE_BLOCKS
+
+const Block blocks[] = {
+	BLOCK(". ~/forge/scripts/cpu.sh",	2,	0),
+	BLOCK(". ~/forge/scripts/ram.sh",	2,	0),
+	BLOCK(". ~/forge/scripts/dte.sh",	10,	0),
+	BLOCK(". ~/forge/scripts/tim.sh",	1,	0),
+	BLOCK(". ~/forge/scripts/bat.sh",	2,	0)
+};
